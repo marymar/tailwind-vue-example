af8b78069a653f9d4e077f31d117c31ce13fbd8e
mary.vokicic@starfinanz.de <mary.vokicic@starfinanz.de>
2020-01-16 22:57:17 +0100

    extract card into separate component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,76 +5,13 @@
     <div class="container mx-auto p-4">
       <div class="flex flex-wrap">
         <div class="w-full sm:w-1/3 pb-2 sm:p-2">
-          <div class="card">
-            <span class="card-badge">
-              <img src="@/assets/mary.png" />
-            </span>
-
-            <h1 class="card-title">
-              Small-Letter
-            </h1>
-            <img
-              src="@/assets/thumbs-up-icon.png"
-              class="mx-auto h-20 my-5 hidden sm:block"
-            />
-            <p class="card-hint">Hinweis....</p>
-            <p class="card-text text-base">
-              dieser text hat eigentlich gar keinen wirklichen inhalt. aber er
-              hat auch keine relevanz, und deswegen ist das egal. er dient
-              lediglich als platzhalter. um mal zu zeigen, wie diese stelle der
-              seite aussieht, wenn ein paar zeilen vorhanden sind. ob sich der
-              text dabei gut fühlt, weiß ich nicht. ich schätze, eher nicht,
-              denn wer fühlt sich
-            </p>
-          </div>
+          <card />
         </div>
         <div class="w-full sm:w-1/3 pb-2 sm:p-2">
-          <div class="card">
-            <span class="card-badge">
-              <img src="@/assets/mary.png" />
-            </span>
-
-            <h1 class="card-title">
-              Small-Letter
-            </h1>
-            <img
-              src="@/assets/thumbs-up-icon.png"
-              class="mx-auto h-20 my-5 hidden sm:block"
-            />
-            <p class="card-hint">Hinweis....</p>
-            <p class="card-text text-base">
-              dieser text hat eigentlich gar keinen wirklichen inhalt. aber er
-              hat auch keine relevanz, und deswegen ist das egal. er dient
-              lediglich als platzhalter. um mal zu zeigen, wie diese stelle der
-              seite aussieht, wenn ein paar zeilen vorhanden sind. ob sich der
-              text dabei gut fühlt, weiß ich nicht. ich schätze, eher nicht,
-              denn wer fühlt sich
-            </p>
-          </div>
+          <card />
         </div>
         <div class="w-full sm:w-1/3 pb-2 sm:p-2">
-          <div class="card">
-            <span class="card-badge">
-              <img src="@/assets/mary.png" />
-            </span>
-
-            <h1 class="card-title">
-              Small-Letter
-            </h1>
-            <img
-              src="@/assets/thumbs-up-icon.png"
-              class="mx-auto h-20 my-5 hidden sm:block"
-            />
-            <p class="card-hint">Hinweis....</p>
-            <p class="card-text">
-              dieser text hat eigentlich gar keinen wirklichen inhalt. aber er
-              hat auch keine relevanz, und deswegen ist das egal. er dient
-              lediglich als platzhalter. um mal zu zeigen, wie diese stelle der
-              seite aussieht, wenn ein paar zeilen vorhanden sind. ob sich der
-              text dabei gut fühlt, weiß ich nicht. ich schätze, eher nicht,
-              denn wer fühlt sich
-            </p>
-          </div>
+          <card />
         </div>
       </div>
     </div>
@@ -82,42 +19,15 @@
 </template>
 
 <script>
+import card from './card.vue'
+
 export default {
+  components: {
+    card,
+  },
   name: 'HelloWorld',
   props: {
     msg: String,
   },
 }
 </script>
-
-<style lang="postcss" scoped>
-.card {
-  @apply border-gray-600 border-solid border rounded-lg p-4 relative;
-}
-
-.card-badge {
-  @apply rounded-full w-20 h-20 overflow-hidden inline-block absolute right-0 top-0 -mr-3 -mt-5 hidden;
-}
-
-.card-hint {
-  @apply text-red-600 text-xs mt-4 mb-2;
-}
-
-.card-title {
-  @apply text-center text-2xl text-blue-100 bg-blue-800 -mx-4 -mt-4 py-5 rounded-t-lg;
-}
-
-.card-text {
-  @apply card-text text-base;
-}
-
-@screen sm {
-  .card-badge {
-    @apply block;
-  }
-
-  .card-hint {
-    @apply mt-0;
-  }
-}
-</style>
---
+++ src/components/card.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="card">
+    <span class="card-badge">
+      <img src="@/assets/mary.png" />
+    </span>
+
+    <h1 class="card-title">Small-Letter</h1>
+    <img
+      src="@/assets/thumbs-up-icon.png"
+      class="mx-auto h-20 my-5 hidden sm:block"
+    />
+    <p class="card-hint">Hinweis....</p>
+    <p class="card-text text-base">
+      dieser text hat eigentlich gar keinen wirklichen inhalt. aber er hat auch
+      keine relevanz, und deswegen ist das egal. er dient lediglich als
+      platzhalter. um mal zu zeigen, wie diese stelle der seite aussieht, wenn
+      ein paar zeilen vorhanden sind. ob sich der text dabei gut fühlt, weiß ich
+      nicht. ich schätze, eher nicht, denn wer fühlt sich
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="postcss" scoped>
+.card {
+  @apply border-gray-600 border-solid border rounded-lg p-4 relative;
+}
+
+.card-badge {
+  @apply rounded-full w-20 h-20 overflow-hidden inline-block absolute right-0 top-0 -mr-3 -mt-5 hidden;
+}
+
+.card-hint {
+  @apply text-red-600 text-xs mt-4 mb-2;
+}
+
+.card-title {
+  @apply text-center text-2xl text-blue-100 bg-blue-800 -mx-4 -mt-4 py-5 rounded-t-lg;
+}
+
+.card-text {
+  @apply card-text text-base;
+}
+
+@screen sm {
+  .card-badge {
+    @apply block;
+  }
+
+  .card-hint {
+    @apply mt-0;
+  }
+}
+</style>

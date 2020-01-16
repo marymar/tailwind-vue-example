2e5ea0eaa064c7ae01c6700d008b3960887bb6f7
mary.vokicic@starfinanz.de <mary.vokicic@starfinanz.de>
2020-01-16 21:02:37 +0100

    add a little mor styling to box

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,14 +2,20 @@
   <div>
     <h1 class="pt-5 pb-10 sm:py-20">{{ msg }}</h1>
 
-    <div class="container mx-auto">
-      <div class="border-gray-600 border-solid border rounded-lg p-4">
+    <div class="container mx-auto p-4">
+      <div
+        class="border-gray-600 border-solid border rounded-lg p-4 overflow-hidden"
+      >
+        <h1
+          class="text-center text-2xl text-blue-100 bg-blue-800 -mx-4 -mt-4 px-4"
+        >
+          Small-Letter
+        </h1>
         <img
           src="@/assets/thumbs-up-icon.png"
-          class="mx-auto h-20 mb-5 hidden sm:block"
+          class="mx-auto h-20 my-5 hidden sm:block"
         />
-        <h1 class="text-center text-2xl text-blue-700">Small-Letter</h1>
-        <p class="text-red-600 text-xs">Hinweis....</p>
+        <p class="text-red-600 text-xs mt-4 mb-2 sm:mt-0">Hinweis....</p>
         <p class="card-text text-base">
           dieser text hat eigentlich gar keinen wirklichen inhalt. aber er hat
           auch keine relevanz, und deswegen ist das egal. er dient lediglich als

07dc374a53fc01c3c8b8baa191a17eb62f291510
mary.vokicic@starfinanz.de <mary.vokicic@starfinanz.de>
2020-01-16 21:00:47 +0100

    add container and simple box

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
-    <h1 class="pb-10">{{ msg }}</h1>
-    <p class="text-red-600">Roter Text</p>
+    <h1 class="pt-5 pb-10 sm:py-20">{{ msg }}</h1>
+
+    <div class="container mx-auto">
+      <div class="border-gray-600 border-solid border rounded-lg p-4">
+        <img
+          src="@/assets/thumbs-up-icon.png"
+          class="mx-auto h-20 mb-5 hidden sm:block"
+        />
+        <h1 class="text-center text-2xl text-blue-700">Small-Letter</h1>
+        <p class="text-red-600 text-xs">Hinweis....</p>
+        <p class="card-text text-base">
+          dieser text hat eigentlich gar keinen wirklichen inhalt. aber er hat
+          auch keine relevanz, und deswegen ist das egal. er dient lediglich als
+          platzhalter. um mal zu zeigen, wie diese stelle der seite aussieht,
+          wenn ein paar zeilen vorhanden sind. ob sich der text dabei gut fühlt,
+          weiß ich nicht. ich schätze, eher nicht, denn wer fühlt sich
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
